1c6c1e82e21b7a4365ff5ea0606f08a231a5d4c7
aouaiti <94193114+aouaiti@users.noreply.github.com>
2021-12-01 10:12:02 +0100

    initial upload 1/12
    my second template

---
+++ 2nd/js test.js
@@ -0,0 +1,21 @@
+// Add Variables Here
+let a=10 , b=20;
+// Ouput
+console.log(+10+20); // Normal Concatenate => 1020
+console.log("Add Code Here"); // Normal Concatenate => String
+console.log("Add Code Here"); // Template Literals Way => 1020
+console.log("Add Code Here"); // Template Literals Way => String
+
+console.log("Add Code Here");
+/*
+  Normal Concatenate
+  20
+  10
+*/
+
+console.log("Add Code Here");
+/*
+  Template Literals Way
+  20
+  10
+*/
